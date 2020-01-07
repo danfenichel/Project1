d1e0345b16c734a7f0963c4e13c87559988654ea
D0491 <dan.fenichel@yahoo.com>
2020-01-07 14:14:54 -0600

    Added functionality to pull Zomato info from API

--- logic.js
+++ logic.js
@@ -12,7 +12,23 @@ $("#submit").on("click", function(event) {
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        console.log(response.location_suggestions[0].id);
+        var cityID = response.location_suggestions[0].id;
+        var queryURL1 = "https://developers.zomato.com/api/v2.1/search?entity_id=" + cityID + "&entity_type=city&count=1&apikey=0be82c0991914e9afe82f36e7fbdf1e6";
+        $.ajax({
+            url: queryURL1,
+            method: "GET"
+        }).then(function (response) {
+            // var randomResult = Math.floor((Math.random() * response.results_found) + 1);
+            // // console.log(randomResult);
+            // var queryURL2 = "https://developers.zomato.com/api/v2.1/search?entity_id=" + cityID + "&entity_type=city&start=0&count=1&apikey=0be82c0991914e9afe82f36e7fbdf1e6";
+            // $.ajax({
+            //     url: queryURL2,
+            //     method: "GET"
+            // }).then(function (response) {
+                console.log(response.restaurants[0].restaurant.name);
+                // console.log(response.restaurants);
+            // })
+        });
     });
 });
 
