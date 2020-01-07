765c894aaa098e3a67090e473085f84a26b937bd
Brad Bryant <bradleybryant86@gmail.com>
2020-01-07 12:52:51 -0600

    Added event.preventdefault to find your new favorite place button

---
+++ logic.js
@@ -0,0 +1,10 @@
+// Dropdown Menu: Selected values stay visible after being clicked by user
+function dropdownValues() {
+var x = document.getElementById("mySelect").value;
+}
+dropdownValues();
+
+// Get results for desired restaurant of brewery selection by clicking this button
+function getResults() {
+// Display restaurant/brewery info
+}
