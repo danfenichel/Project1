f024d85dfc8a5dbbd56ee381a46eb6aa5dbf4775
D0491 <dan.fenichel@yahoo.com>
2020-01-09 10:13:34 -0600

    Made minor changes to code

--- logic.js
+++ logic.js
@@ -6,11 +6,17 @@ var selectTypeEl = $("#type-select");
 var submitBtn = $("#submit");
 var errorText = $("<p>").text("Please fill out all required fields!");
 
-$("#submit").on("click", function(event) {
+
+// HIDING FOOD DROP DOWN WHEN THEY CLICK BREWERY - FIGURE OUT WHY NOT WORKING
+if ($(selectTypeEl).val() === "Brewery") {
+    $("#food-select").attr("style", "display:none");
+};
+// On click event for Submit button
+$("#submit").on("click", function (event) {
     event.preventDefault();
 
     // Shows Error Text if required fields aren't populated
-    if(cityInputEl.val() === "" || selectStateEl.val() === "Select State" || selectTypeEl.val() === "Select Restaurant or Brewery"){
+    if (cityInputEl.val() === "" || selectStateEl.val() === "Select State" || selectTypeEl.val() === "Select Restaurant or Brewery") {
         errorText.attr("style", "color: red");
         formEl.append(errorText);
         return;
@@ -19,24 +25,27 @@ $("#submit").on("click", function(event) {
     errorText.remove();
     // Changes text of submit button to tell users to keep cycling through to next options
     $("#submit").text("Not Satisfied? Thank you, NEXT!");
-    // AJAX query to get city code based on user input (city code needed to do further search)
-    var city = cityInputEl.val();
-    var queryURL = "https://developers.zomato.com/api/v2.1/cities?q=" + city + "&count=1&apikey=0be82c0991914e9afe82f36e7fbdf1e6";
+    // AJAX query to get city code based on user input for RESTAURANTS (city code needed to do further search)
+    if (selectTypeEl.val() === "Restaurant") {
+        var city = cityInputEl.val();
+        var queryURL = "https://developers.zomato.com/api/v2.1/cities?q=" + city + "&count=1&apikey=0be82c0991914e9afe82f36e7fbdf1e6";
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        // AJAX query using city code pulled from above to search random restaurant options
-        var cityID = response.location_suggestions[0].id;
-        var cuisineID = $("#food-select").val();
-        var queryURL1 = "https://developers.zomato.com/api/v2.1/search?entity_id=" + cityID + "&entity_type=city&count=20&cuisines=" + cuisineID + "&apikey=0be82c0991914e9afe82f36e7fbdf1e6";
         $.ajax({
-            url: queryURL1,
+            url: queryURL,
             method: "GET"
         }).then(function (response) {
+            // AJAX query using city code pulled from above to search random restaurant options
+            var cityID = response.location_suggestions[0].id;
+            var cuisineID = $("#food-select").val();
+            var queryURL1 = "https://developers.zomato.com/api/v2.1/search?entity_id=" + cityID + "&entity_type=city&count=20&cuisines=" + cuisineID + "&apikey=0be82c0991914e9afe82f36e7fbdf1e6";
+            $.ajax({
+                url: queryURL1,
+                method: "GET"
+            }).then(function (response) {
+                // To pull random option from list of results (only pulls 20 at a time)
                 var randomNum = Math.floor(Math.random() * 20 + 1);
-                
+
+                // Creates new elements with data from AJAX pull
                 var restName = $("<h5>").text(response.restaurants[randomNum].restaurant.name);
                 var restType = $("<h6>").text("Type of Food: " + response.restaurants[randomNum].restaurant.cuisines)
                 var restAddr = $("<h6>").text("Address: " + response.restaurants[randomNum].restaurant.location.address);
@@ -48,12 +57,14 @@ $("#submit").on("click", function(event) {
                 restUrl.attr("target", "_blank");
                 var lineBreak = $("<br>");
 
+                // Empties results every time button is clicked
                 $(".resultBox").empty();
-                
+
+                // Appends results to page
                 $(".resultBox").append(restName, restType, restAddr, restPhone, restHours, restRtng, restUrl, lineBreak);
+            });
         });
-    });
-    // Add functionality to STOP submit button from working after clicking once. Tried "return" - did not work
+    };
 });
 
 
